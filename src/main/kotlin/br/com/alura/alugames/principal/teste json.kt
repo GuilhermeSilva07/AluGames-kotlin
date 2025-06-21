cdb26d43cef474606600e2b8588bf8615afac835
Guilherme SIlva <gs2281172@gmail.com>
2025-06-21 19:15:07 -0300

    Subindo testejson

--- src/main/kotlin/br/com/alura/alugames/principal/teste json.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/teste json.kt
@@ -1,2 +1,11 @@
 package br.com.alura.alugames.principal
 
+import br.com.alura.alugames.serviços.ConsumoApi
+
+fun main (){
+
+    val consumo = ConsumoApi()
+    val ListaGamers = consumo.buscaGamer()
+
+    println(ListaGamers)
+}
