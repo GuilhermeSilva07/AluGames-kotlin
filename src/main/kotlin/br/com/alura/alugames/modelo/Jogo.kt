d82441b04f9e42ce2b2b4b6b45eed7e607b66ab3
Guilherme SIlva <gs2281172@gmail.com>
2025-06-28 18:02:57 -0300

    Correçõo na classe jogo

--- src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
@@ -1,29 +1,14 @@
 package br.com.alura.alugames.modelo
 
-data class Jogo(val titulo: String, val capa: String)  //Construtor
-    {
-
-    //Atributos do jogo
-
-    /* Variaveis declaradas como "Var" permitem alteração nos valores inseridos" */
-
+data class Jogo(val titulo: String,
+                val capa: String) {
     var descricao:String? = null
     var preco = 0.0
 
     constructor(titulo: String, capa: String, preco: Double, descricao: String):
-            this(titulo, capa){
-                this.preco=preco
-                this.descricao=descricao
-            }
-        data class Jogo(val titulo:String,
-                        val capa:String) {
-            var descricao: String? = null
-            var preco = 0.0
-
-            constructor(titulo: String, capa: String, preco: Double, descricao: String):
-                    this(titulo, capa) {
-                this.preco = preco
-                this.descricao = descricao
+                    this(titulo, capa){
+                this.preco= preco
+                this.descricao= descricao
             }
 
             override fun toString(): String {
